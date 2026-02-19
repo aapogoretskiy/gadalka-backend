aa7e3b9b972e90264e9f605ac227b9c5decd707e
Александр Погорецкий <aapogor8@mts.ru>
2026-02-19 21:24:30 +0300

    init msg

---
+++ src/main/java/ru/sapa/gadalka_backend/GadalkaBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.sapa.gadalka_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadalkaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadalkaBackendApplication.class, args);
+	}
+
+}
